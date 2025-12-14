9f41b98b34486c2efc1d1ecccff0feca2e909a34
Josua <101020608+diggejos@users.noreply.github.com>
2025-12-14 14:41:37 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -8,8 +10,11 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
+x
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
