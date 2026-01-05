cc6c6e264c3942d87af50ebe794ec39c3d01d8f8
Josua <101020608+diggejos@users.noreply.github.com>
2026-01-05 14:03:51 +0100

    Integrate BrowserRouter for routing in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "./index.css";
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+<React.StrictMode>
+<BrowserRouter>
+<App />
+</BrowserRouter>
+</React.StrictMode>
 );
